1ef3587d3f1ce0507f94c531dd0d780c5732e163
Foad Davoodi <myf.davoodi@gmail.com>
2024-10-14 21:47:24 +0330

    Add files via upload

---
+++ bot/core/tasks.py
@@ -0,0 +1,174 @@
+tasks = {
+  1: {
+    "id": 1,
+    "type": "welcome",
+    "title": "welcome",
+    "price": 10,
+    "icon": "welcome",
+    "repeatable": False,
+    "status": "ready-to-claim"
+  },
+  2: {
+    "id": 2,
+    "type": "checkin",
+    "title": "daily Check-In",
+    "price": 5,
+    "icon": "checkin",
+    "status": "completed",
+    "repeatable": True,
+    "rewardCreatedAt": "2024-10-12T13:32:18.179Z"
+  },
+  3: {
+    "id": 3,
+    "type": "watch",
+    "title": "watch 10 Coub",
+    "price": 20,
+    "icon": "video-octagon",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  4: {
+    "id": 4,
+    "type": "share",
+    "title": "share 3 Coub",
+    "price": 15,
+    "icon": "share",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  6: {
+    "id": 6,
+    "type": "twitter",
+    "title": "follow X",
+    "price": 10,
+    "icon": "twitter",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  7: {
+    "id": 7,
+    "type": "telegram",
+    "title": "follow Tg",
+    "price": 10,
+    "icon": "telegram",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  9: {
+    "id": 9,
+    "type": "youtube",
+    "title": "follow Youtube",
+    "price": 10,
+    "icon": "youtube",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  8: {
+    "id": 8,
+    "type": "onboarding",
+    "title": "completed Tasks",
+    "price": 100,
+    "icon": "onboarding",
+    "repeatable": False,
+    "status": "ready-to-start"
+  },
+  12: {
+    "id": 12,
+    "type": "watch-25-random",
+    "title": "Watch coubs In Random 25",
+    "price": 10,
+    "icon": "watch",
+    "status": "started",
+    "repeatable": True
+  },
+  13: {
+    "id": 13,
+    "type": "watch-25-rising",
+    "title": "Watch coubs In Rising 25",
+    "price": 10,
+    "icon": "watch",
+    "status": "started",
+    "repeatable": True
+  },
+  14: {
+    "id": 14,
+    "type": "watch-25-recommendations",
+    "title": "Watch coubs In For You 25",
+    "price": 10,
+    "icon": "watch",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+  15: {
+    "id": 15,
+    "type": "like-5-random",
+    "title": "Like in Random 5",
+    "price": 5,
+    "icon": "like",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+  16: {
+    "id": 16,
+    "type": "like-5-rising",
+    "title": "like in Rising 5",
+    "price": 5,
+    "icon": "like",
+    "status": "started",
+    "repeatable": True
+  },
+  17: {
+    "id": 17,
+    "type": "like-5-recommendations",
+    "title": "like in For You 5",
+    "price": 5,
+    "icon": "like",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+  18: {
+    "id": 18,
+    "type": "share-wtf-video",
+    "title": "share WTF Story",
+    "price": 30,
+    "icon": "share-coub",
+    "status": "ready-to-start",
+    "repeatable": False
+  },
+  19: {
+    "id": 19,
+    "type": "share-story",
+    "title": "share Story",
+    "price": 5,
+    "icon": "share-story",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+  25: {
+    "id": 25,
+    "type": "edit-channel",
+    "title": "edit Channel",
+    "price": 5,
+    "icon": "coub",
+    "status": "ready-to-start",
+    "repeatable": False
+  },
+  26: {
+    "id": 26,
+    "type": "like-5-sports",
+    "title": "like Sports",
+    "price": 5,
+    "icon": "like",
+    "status": "ready-to-start",
+    "repeatable": True
+  },
+ 32: {
+    "id": 32,
+    "type": "coub-app-trending",
+    "title": "#CoubTrending Like & RT",
+    "price": 10,
+    "icon": "twitter",
+    "status": "ready-to-start",
+    "repeatable": False
+  }
+}
